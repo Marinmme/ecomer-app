e333fd75d14a6228443756858baa3d1ff6941611
Marinmme <nguyenconghainam2@gmail.com>
2022-10-06 10:39:18 +0700

    Login Page JSX

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,8 +1,40 @@
 import React from 'react'
-
+import styles from './auth.module.scss'
+import login from '../../assets/login.png'
+import {Link} from 'react-router-dom'
+import { FaGoogle } from 'react-icons/fa'
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+      <section className={`container ${styles.auth}`}>
+<div className={styles.img}>
+<img src={login} alt='' width="400"></img>
+</div>
+<div className={styles.form}>
+<h2>Login</h2>
+<form>
+<input type="text" placeholder="Email" required></input>
+<input type="password" placeholder="password" required></input>
+<button className="--btn --btn-primary --btn-block">
+  Login
+</button>
+<div className={styles.links}>
+<Link to='/reset'>Reset Password</Link>
+</div>
+<p>-- or --</p>
+</form>
+<button className="--btn --btn-danger --btn-block">
+<FaGoogle color="#fff"></FaGoogle>
+  Login With Google
+</button>
+<span>
+Done'have an account?
+<Link to='/register'> Register</Link>
+</span>
+
+</div>
+      </section>
+    </>
   )
 }
 
