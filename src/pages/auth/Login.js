7684c068663fadfab18300efc655f7e2cf1a3237
Marinmme <nguyenconghainam2@gmail.com>
2022-10-11 12:37:45 +0700

    Sign in with email and Password

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,12 +1,40 @@
-import React from 'react'
+import React,{useState} from 'react'
 import styles from './auth.module.scss'
 import login from '../../assets/login.png'
-import {Link} from 'react-router-dom'
+import {Link,useNavigate} from 'react-router-dom'
 import { FaGoogle } from 'react-icons/fa'
 import Card from '../../components/card/Card'
+import {  signInWithEmailAndPassword } from "firebase/auth";
+import {auth} from '../../firebase/config'
+import { ToastContainer, toast } from 'react-toastify';
+import Loader from '../../components/loader/Loader'
 const Login = () => {
+  const [email,setEmail]=useState("")
+  const [password,setPassword]=useState("")
+  const [iLoading,setILoading] = useState(false);
+  const navigate = useNavigate()
+  const handleLogin = (e) => {
+    e.preventDefault();
+    setILoading(true)
+
+    signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+
+    const user = userCredential.user;
+   setILoading(false)
+   navigate("/")
+   toast.success("Login successful")
+  })
+  .catch((error) => {
+    toast.error(error.message)
+    setILoading(false)
+  })
+  }
   return (
+
     <>
+    <ToastContainer></ToastContainer>
+        {iLoading && <Loader></Loader>}
       <section className={`container ${styles.auth}`}>
 <div className={styles.img}>
 <img src={login} alt='' width="400"></img>
@@ -15,10 +43,16 @@ const Login = () => {
 <div className={styles.form}>
 <h2>Login</h2>
 
-<form>
-<input type="text" placeholder="Email" required></input>
-<input type="password" placeholder="password" required></input>
-<button className="--btn --btn-primary --btn-block">
+<form onSubmit={handleLogin}>
+<input type="text" placeholder="Email" required
+value={email} onChange={(e)=>setEmail(e.target.value)}
+></input>
+<input type="password" placeholder="password" required
+value={password} onChange={(e)=>setPassword(e.target.value)}
+></input>
+<button className="--btn --btn-primary --btn-block"
+type="submit"
+>
   Login
 </button>
 <div className={styles.links}>
